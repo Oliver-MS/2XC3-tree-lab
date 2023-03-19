8235ce2071080965c6340070bc041603961377e6
Selim Abdel Razek <srazek2003@hotmail.com>
2023-03-19 16:08:50 -0400

    implemented xc3tree

---
+++ xc3tree.py
@@ -0,0 +1,23 @@
+class XC3Node:
+    def __init__(self, degree):
+        self.degree = degree
+        self.children = [(XC3Node(0) if i <= 2 else XC3Node(i-2)) for i in range(1, self.degree+1)]
+    def is_leaf(self):
+        return self.children == []
+    def rightmost_child(self):
+        return self.children[-1]
+
+class XC3Tree:
+    def __init__(self, degree):
+        self.root = XC3Node(degree)
+    def is_empty(self):
+        return self.root == None
+    def get_height(self):
+        if self.root.degree == 0:
+            return 0
+        return self._get_height(self.root)
+    
+    def _get_height(self, node):
+        if node.degree == 0:
+            return 1
+        return 1 + self._get_height(node.rightmost_child())
